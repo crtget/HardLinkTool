1fdc9706c3e85e010761711e5cf3afed61929e6a
crtget <ctrget@gmail.com>
2020-02-23 20:47:05 +0800

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,24 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnlink = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnbrowser2 = new System.Windows.Forms.Button();
+            this.tbxlink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnbrowser1 = new System.Windows.Forms.Button();
+            this.tbxtarget = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnlink);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnbrowser2);
+            this.panel1.Controls.Add(this.tbxlink);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnbrowser1);
+            this.panel1.Controls.Add(this.tbxtarget);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 266);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnlink
+            // 
+            this.btnlink.Location = new System.Drawing.Point(161, 139);
+            this.btnlink.Name = "btnlink";
+            this.btnlink.Size = new System.Drawing.Size(109, 34);
+            this.btnlink.TabIndex = 8;
+            this.btnlink.Text = "开始链接";
+            this.btnlink.UseVisualStyleBackColor = true;
+            this.btnlink.Click += new System.EventHandler(this.btnlink_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(17, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(399, 59);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "硬连接可复制文件并不占用磁盘空间 但修改会文件会同步到文件本体和所有链接上，覆盖操作则不会引起源文件变动。可直接将需链接文件或文件夹拖放至本程序上即可自动链接。 " +
+    "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "链接路径(必须与源路径在同一文件夹下)";
+            // 
+            // btnbrowser2
+            // 
+            this.btnbrowser2.Location = new System.Drawing.Point(381, 98);
+            this.btnbrowser2.Name = "btnbrowser2";
+            this.btnbrowser2.Size = new System.Drawing.Size(54, 21);
+            this.btnbrowser2.TabIndex = 5;
+            this.btnbrowser2.Text = "浏览";
+            this.btnbrowser2.UseVisualStyleBackColor = true;
+            this.btnbrowser2.Click += new System.EventHandler(this.btnbrowser2_Click);
+            // 
+            // tbxlink
+            // 
+            this.tbxlink.Location = new System.Drawing.Point(3, 98);
+            this.tbxlink.Name = "tbxlink";
+            this.tbxlink.Size = new System.Drawing.Size(372, 21);
+            this.tbxlink.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "源路径(支持拖放多文件或文件夹)";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(394, 245);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(41, 12);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnbrowser1
+            // 
+            this.btnbrowser1.Location = new System.Drawing.Point(381, 32);
+            this.btnbrowser1.Name = "btnbrowser1";
+            this.btnbrowser1.Size = new System.Drawing.Size(54, 21);
+            this.btnbrowser1.TabIndex = 1;
+            this.btnbrowser1.Text = "浏览";
+            this.btnbrowser1.UseVisualStyleBackColor = true;
+            this.btnbrowser1.Click += new System.EventHandler(this.btnbrowser1_Click);
+            // 
+            // tbxtarget
+            // 
+            this.tbxtarget.AllowDrop = true;
+            this.tbxtarget.Location = new System.Drawing.Point(3, 32);
+            this.tbxtarget.Name = "tbxtarget";
+            this.tbxtarget.Size = new System.Drawing.Size(372, 21);
+            this.tbxtarget.TabIndex = 0;
+            this.tbxtarget.DragDrop += new System.Windows.Forms.DragEventHandler(this.tbxtarget_DragDrop);
+            this.tbxtarget.DragEnter += new System.Windows.Forms.DragEventHandler(this.tbxtarget_DragEnter);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(660, 381);
+            this.ClientSize = new System.Drawing.Size(464, 289);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MainForm";
+            this.Text = "硬链接工具";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnbrowser2;
+        private System.Windows.Forms.TextBox tbxlink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnbrowser1;
+        private System.Windows.Forms.TextBox tbxtarget;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnlink;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -54,7 +54,7 @@ namespace HardLinkTool
         }
 
 
-        public void MakeHardLink(string link, string target)
+        public bool MakeHardLink(string link, string target)
         {
             var d = Path.GetDirectoryName(link);
 
@@ -63,15 +63,15 @@ namespace HardLinkTool
                 Directory.CreateDirectory(d);
             }
 
-            CreateHardLink(link, target, IntPtr.Zero);
+            return CreateHardLink(link, target, IntPtr.Zero);
         }
 
 
-        public void MakeDirHardLink(string link, string target)
+        public bool MakeDirHardLink(string link, string target)
         {
             List<string> files = new List<string>();
-
             GetPathFiles(target, ref files);
+            var result = true;
 
 
             foreach (var f in files)
@@ -84,8 +84,11 @@ namespace HardLinkTool
                     Directory.CreateDirectory(d);
                 }
 
-                MakeHardLink(ff, f);
+                if (!MakeHardLink(ff, f))
+                    result = false;
             }
+
+            return result;
         }
 
 
@@ -96,9 +99,11 @@ namespace HardLinkTool
         {
             InitializeComponent();
 
+            var result = true;
+
             foreach(var a in args)
             {
-
+                
                 if (!File.Exists(a) && !Directory.Exists(a))
                     continue;
 
@@ -106,29 +111,182 @@ namespace HardLinkTool
 
                 if ((new FileInfo(fname).Attributes & FileAttributes.Directory) == FileAttributes.Directory)
                 {
-                    MakeDirHardLink(new DirectoryInfo(fname).Parent.FullName + "\\" + new DirectoryInfo(fname).Name + "_Linked", a);
+                    if (!MakeDirHardLink(new DirectoryInfo(fname).Parent.FullName + "\\" + new DirectoryInfo(fname).Name + "_Linked", a))
+                        result = false;
                     
                 }
                 else
                 {
-                    MakeHardLink(Path.GetDirectoryName(fname) + "\\" + Path.GetFileNameWithoutExtension(fname) + "_Linked" + Path.GetExtension(fname), a);
+                    if (!MakeHardLink(Path.GetDirectoryName(fname) + "\\" + Path.GetFileNameWithoutExtension(fname) + "_Linked" + Path.GetExtension(fname), a))
+                        result = false;
                 }
             }
 
-  
+
+
+            if (args.Length > 0)
+            {
+
+                if (!result)
+                    ShowResult(result);
+
+                this.Close();
+            }
+                
+
  
         }
+
+
+
+        public void ShowResult(bool b)
+        {
+            if (b)
+            {
+                MessageBox.Show(this, "操作成功完成！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(this, "操作完成，有错误发生！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
 
         private void MainForm_Load(object sender, EventArgs e)
         {
 
 
-
+          
             //
 
 
 
 
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/crtget/HardLinkTool");
+        }
+
+
+        private void tbxtarget_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+        private void tbxtarget_DragDrop(object sender, DragEventArgs e)
+        {
+            tbxtarget.Text = "";
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (var f in files)
+            {
+
+                tbxtarget.Text += f;
+
+                if (f != files[files.Length - 1])
+                    tbxtarget.Text += "|";
+
+
+            }
+        }
+
+        private void btnbrowser1_Click(object sender, EventArgs e)
+        {
+            var fd = new FolderBrowserDialog();
+            fd.Description = "请选择源路径";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                tbxtarget.Text = fd.SelectedPath;
+            }
+
+        }
+
+        private void btnbrowser2_Click(object sender, EventArgs e)
+        {
+            var fd = new FolderBrowserDialog();
+            fd.Description = "请选链接路径";
+
+            var target = tbxtarget.Text;
+
+            if (target.Trim() != "")
+            {
+                fd.SelectedPath = target.Substring(0, 3); ;
+            }
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                tbxlink.Text = fd.SelectedPath;
+            }
+        }
+
+        private void btnlink_Click(object sender, EventArgs e)
+        {
+            var target = tbxtarget.Text;
+            var link = tbxlink.Text;
+            var result = true;
+            string[] files = null;
+
+            if (target.Trim() == "" || link.Trim() == "")
+                return;
+
+
+            if (target.IndexOf("|") >= 0)
+            {
+                files = target.Split('|');
+            }
+            else
+            {
+                files = new string[] { target };
+            }
+
+            if (Path.GetPathRoot(link) != Path.GetPathRoot(files[0]))
+            {
+                MessageBox.Show(this, "硬链接路径必须在同一驱动器下！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            foreach (var f in files)
+            {
+
+                if (!File.Exists(f) && !Directory.Exists(f))
+                    continue;
+
+
+                if ((new FileInfo(f).Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    MakeDirHardLink(link + "\\" + new DirectoryInfo(f).Name + "\\", f);
+                }
+                else
+                {
+
+                    if (!MakeHardLink(link + "\\" + Path.GetFileName(f), f))
+                        result = false;
+
+                }
+            }
+
+            ShowResult(result);
+
+
+
+        }
+
+
+
+
     }
+
 }
